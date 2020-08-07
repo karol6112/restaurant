59eb158d65c0221a956b1f1e00d7097970459bcd
Karol <karol.kolerzak@gmail.com>
2020-08-07 12:45:22 +0200

    fix bill printing function

--- restaurant/order/views.py
+++ restaurant/order/views.py
@@ -25,13 +25,15 @@ class BillViewSet(viewsets.ModelViewSet):
     def generate_bill(self, request, **kwargs):
         bill = self.get_object()
         orders = bill.orders()
+        txt = []
 
-        txt = "Position    Amount    Price    Total    Price\n"
         for name, amount in orders.items():
             position = Position.objects.get(name=name)
             price = position.price
-            txt += f"* {name} - {amount} - {price} - {amount * price}\n"
-        txt += f"Total Price: {bill.total_price()}"
+            body = f"# {name}   {amount}   {price}  {amount * price}"
+            txt.append(body)
+        price = f"Total Price: {bill.total_price()}"
+        txt.append(price)
         self.print_bill(txt, bill.pk)
 
         bill.active = False
@@ -44,6 +46,10 @@ class BillViewSet(viewsets.ModelViewSet):
         pdf = FPDF()
         pdf.add_page()
         pdf.set_xy(0, 0)
-        pdf.set_font('arial', 'B', 13.0)
-        pdf.cell(ln=0, h=5.0, align='L', w=0, txt=txt, border=0)
+        pdf.set_font('arial', 'B', 10.0)
+        top = "Position    Amount    Price    Final Price\n"
+        pdf.cell(ln=10, h=5.0, align='C', w=0, txt=top, border=0)
+        pdf.set_font('arial', '', 10.0)
+        for x in txt:
+            pdf.cell(ln=5, h=5.0, align='C', w=0, txt=x, border=0)
         pdf.output(f'bill{pk}.pdf', 'F')
